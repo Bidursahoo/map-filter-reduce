c1ff73a1fe0cf955bd6d84f5687f09844c5b78a7
Bidursahoo <bibhabsahoobidur@gmail.com>
2023-06-14 20:54:35 +0530

    map vs for each

--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 var numbers = [3, 56, 2, 48, 5];
 
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   
   </React.StrictMode>
 );
+
+
+
+//diffrence between for each and map
+
+
+//map
+function doubleM(x){
+  return x*2;
+}
+var newNum = numbers.map(doubleM);
+
+var newNumber = [];
+//For Each Example
+
+numbers.forEach(function(x){
+  newNumber.push(x*2);
+})
+
+
 
 
 //Map -Create a new array by doing something with each item in an array.
